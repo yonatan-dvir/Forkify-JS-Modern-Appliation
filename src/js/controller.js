989423c4bd5039003631c0c631003c1d416775a6
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-03-24 12:41:24 +0200

    Added the recipe rendering to the showRecipe function, a renderSpinner function for when the page is loading

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -14,8 +18,23 @@ console.log('first');
 
 ///////////////////////////////////////
 
+// Render a spinner while ther page is loading
+const renderSpinner = function (parentElement) {
+  const markup = `<div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>`;
+  parentElement.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
+    const recipeContainer = document.querySelector('.recipe');
+    // Loading recipe
+    // Remove the recipeContainer ineerHtml (The "Start by searching for a recipe or an ingredient. Have fun!" message") and put loading icon instead.
+    recipeContainer.innerHTML = '';
+    renderSpinner(recipeContainer);
     const response = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb34'
     );
@@ -32,9 +51,128 @@ const showRecipe = async function () {
       sourceUrl: recipe.source_url,
       image: recipe.image_url,
       servings: recipe.servings,
-      ingerdeints: recipe.ingerdients,
+      ingredients: recipe.ingredients,
     };
     console.log(recipe);
+
+    //Rendering recipe
+    const markup = `
+        <figure class="recipe__fig">
+        <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${recipe.title}</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${
+            recipe.servings
+          }</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated">
+          <svg>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+        <button class="btn--round">
+          <svg class="">
+            <use href="${icons}#icon-bookmark-fill"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${recipe.ingredients
+            .map(ingredient => {
+              return `<li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="${icons}#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${ingredient.quantity}</div>
+            <div class="recipe__description">
+              <span class="recipe__unit">${ingredient.unit}</span>
+              ${ingredient.description}
+            </div>
+          </li>`;
+            })
+            .join('')}
+          <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="${icons}#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">1000</div>
+            <div class="recipe__description">
+              <span class="recipe__unit">g</span>
+              pasta
+            </div>
+          </li>
+
+          <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="${icons}#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">0.5</div>
+            <div class="recipe__description">
+              <span class="recipe__unit">cup</span>
+              ricotta cheese
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${
+            recipe.publisher
+          }</span>. Please check out
+          directions at their website.
+        </p>
+        <a
+          class="btn--small recipe__btn"
+          href="${recipe.sourceUrl}"
+          target="_blank"
+        >
+          <span>Directions</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </div>
+      `;
+    // Insert the recipe content markup
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     alert(err);
   }
