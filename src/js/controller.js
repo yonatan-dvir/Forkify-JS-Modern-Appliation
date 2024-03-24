4c99aaa3ef689d3243925360b41df6fc61d970d7
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-03-24 10:42:44 +0200

    Created the showRecipe function (For now it shows only one specipfic recipe, not given by the user...)

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,36 @@ const timeout = function (s) {
   });
 };
 
+console.log('first');
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb34'
+    );
+    const data = await response.json();
+    // Throw an error in case of trying to fetch to unvalid url
+    if (!response.ok) throw new Error(`${data.message}`);
+    // Create an easy-ro-read object to the recipe we got
+    let { recipe } = data.data;
+    recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      ingerdeints: recipe.ingerdients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
